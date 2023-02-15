5f135bb3e0c257d84c01e3b14b1650b4e7d1e42d
yephonekyaw <yephonekyaw231202@gmail.com>
2023-02-15 19:35:11 +0630

    bug fixed

--- pinmage/pins/authentication.py
+++ pinmage/pins/authentication.py
@@ -6,7 +6,7 @@ def create_profile(backend, user, *args, **kwargs):
     """
         Create user profile for social authentication
     """
-    Profile.objects.get_or_create(user=user, photo='default_profile.png')
+    Profile.objects.get_or_create(user=user)
     # add @ to username
     user.username = '@' + user.username if not user.username.startswith('@') else user.username
 
